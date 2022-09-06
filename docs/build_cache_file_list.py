cc944ed6e34d1ecb9b57f4743859b07bbd7e60a8
UnacceptableBehaviour <fernandez.simon@gmail.com>
2022-09-06 19:03:57 +0100

    simplify build_cache_file_list.py

--- docs/build_cache_file_list.py
+++ docs/build_cache_file_list.py
@@ -2,54 +2,28 @@
 from pathlib import Path
 from pprint import pprint
 
-# source = ''       # when using root - check
-source = 'docs'     # when using /docs as source - removes docs from the path
 repo_name = 'paycheck'
-project_root = Path('/Users/simon/a_syllabus/lang/html_css_js/paycheck/docs/static')
+project_root = Path('/Users/simon/a_syllabus/lang/html_css_js/paycheck/docs')
+do_not_add_to_cache = ['service_worker.js','build_cache_file_list.py']
 
 # const FILES_TO_CACHE = [
 #   '/static/offline.html',
 # ];
 
-def get_list_of_potential_files_to_cache(search_path, cache_root):    
-    paths = []
-    dir_before_root = str(search_path).split('/')[str(search_path).split('/').index(cache_root)-1]
-    
-    print(dir_before_root)
-    print(search_path)
-    
-    print('const FILES_TO_CACHE = [')
-    
-    for p in search_path.glob('**/*'):
-        path = str(p)
-        if 'node_modules' in path or 'scratch' in path or p.is_dir() or '.DS_Store' in path: continue
-        comps = str(p).split(dir_before_root)
-        cache_targets = Path(comps.pop())
-        #non of comented work - w
-        # root_cache = Path('js_canvas_IO_docs').joinpath(comps.pop() )
-        #root_cache = PurePath('/',repo_name).joinpath(comps.pop())
-        #root_cache = Path('/').joinpath(comps.pop(), 'js_canvas_IO_docs', 'twat' )
-        #root_cache = Path('/').joinpath('js_canvas_IO_docs', comps.pop() )
-        #root_cache = Path('/').joinpath('js_canvas_IO_docs', str(comps.pop()) )
-        # root_cache = Path('js_canvas_IO_docs').joinpath(str(comps.pop()) )
-        # print(f"  '{root_cache}',")
-        #print(f"ct:{cache_targets}<")
-        print(f"  '/{repo_name}{cache_targets}',")
-       
-        
-        #print(f"  '/{cache_targets}',")
-        paths.append(cache_targets)    
-    
-    print('];')
-    
-    return paths
-
-def main():
-    pass
-
-
 if __name__ == '__main__':
     
-    paths = get_list_of_potential_files_to_cache(project_root, 'static')
+    search_path = project_root
+    prepend = ''
+    #prepend = f"/{repo_name}"
     
+    print("const FILES_TO_CACHE = [\n  '/',")
     
+    for p in project_root.glob('**/*'):
+        path = str(p)
+        if 'node_modules' in path or 'scratch' in path or p.is_dir() or '.DS_Store' in path: continue
+        if p.name in do_not_add_to_cache: continue
+        #print(p)
+        #print(path.replace(str(project_root),''))
+        print(f"  '{prepend}{path.replace(str(project_root),'')}',")
+    
+    print('];')
