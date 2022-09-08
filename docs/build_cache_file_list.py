fa48681f181dea1c22137571da5b33ed4b86972c
UnacceptableBehaviour <fernandez.simon@gmail.com>
2022-09-08 10:56:15 +0100

    remove cruft

--- docs/build_cache_file_list.py
+++ docs/build_cache_file_list.py
@@ -9,21 +9,19 @@ do_not_add_to_cache = ['service_worker.js','build_cache_file_list.py']
 # const FILES_TO_CACHE = [
 #   '/static/offline.html',
 # ];
+    
+search_path = project_root
+prepend = ''
+prepend = f"/{repo_name}"
 
-if __name__ == '__main__':
-    
-    search_path = project_root
-    prepend = ''
-    prepend = f"/{repo_name}"
-    
-    print("const FILES_TO_CACHE = [\n  '/',")
-    
-    for p in project_root.glob('**/*'):
-        path = str(p)
-        if 'node_modules' in path or 'scratch' in path or p.is_dir() or '.DS_Store' in path: continue
-        if p.name in do_not_add_to_cache: continue
-        #print(p)
-        #print(path.replace(str(project_root),''))
-        print(f"  '{prepend}{path.replace(str(project_root),'')}',")
-    
-    print('];')
+print("const FILES_TO_CACHE = [\n  '/',")
+
+for p in project_root.glob('**/*'):
+    path = str(p)
+    if 'node_modules' in path or 'scratch' in path or p.is_dir() or '.DS_Store' in path: continue
+    if p.name in do_not_add_to_cache: continue
+    #print(p)
+    #print(path.replace(str(project_root),''))
+    print(f"  '{prepend}{path.replace(str(project_root),'')}',")
+
+print('];')
