0f6a63ec020c80aeb0acfe0325c5c74916466795
UnacceptableBehaviour <fernandez.simon@gmail.com>
2022-09-17 19:35:59 +0100

    Add AL anual leave to dropdown

--- docs/service_worker.js
+++ docs/service_worker.js
@@ -13,7 +13,7 @@
 
 const KEY_SW_INFO = 'sw_info';   // must match in paycheck.js!
 
-let verion_number_passed_in = '00.13';  // < - - - - - - - - - - - - - - - - - - - - - - //
+let verion_number_passed_in = '00.14';  // < - - - - - - - - - - - - - - - - - - - - - - //
                                                                                           //
 const CACHE_NAME = `paycheck-gitio-cache_${verion_number_passed_in}`;                     //
                                                                                           //
